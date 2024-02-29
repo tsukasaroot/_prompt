9cf9113f8e8baa045470d8ff955428645f7ab8c1
Islanzadie <islan@Islan>
2024-02-29 11:08:36 +0100

    added confirm method

--- _prompt.js
+++ _prompt.js
@@ -25,6 +25,30 @@ class Prompt {
                 dialogue_window = null;
             });
         });
+
+        ipcMain.on('confirm', (event, args) => {
+            let dialogue_window = new BrowserWindow({
+                width: 400, height: 200, show: false, webPreferences: {
+                    nodeIntegration: true, contextIsolation: false
+                }, autoHideMenuBar: true
+            });
+
+            dialogue_window.loadURL(`file://${__dirname}/_confirm.html?field_name=${args}`).then(r => {
+                dialogue_window.show();
+
+                ipcMain.on('confirm-reply-child', (event_child, args) => {
+                    event.returnValue = args;
+                });
+            });
+
+            dialogue_window.on('close', () => {
+                dialogue_window = null;
+            });
+
+            dialogue_window.on('closed', () => {
+                dialogue_window = null;
+            });
+        });
     }
 }
 
